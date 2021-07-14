0dba0416bc2f263584b824cd2b2f218fdb0d5ec0
Hewitt McGaughey <hewittmcgaughey@gmail.com>
2021-07-14 18:45:46 -0400

    add verify_cell, need to test further to confirm working; move unit tests into test mod

--- src/main.rs
+++ src/main.rs
@@ -54,6 +54,37 @@ impl Sudoku {
         }
         ret
     }
+
+    // Verify whether an individual cell is valid.
+    fn verify_cell(&self, row:usize, col:usize) -> bool {
+        let cell_val = self.board[row][col];
+
+        // Check row/col
+        for i in usize::from(NUM_MIN)..usize::from(NUM_MAX) {
+            // Row
+            if i != col && self.board[row][i] == cell_val {
+                return false;
+            }
+            
+            // Col
+            if i != row && self.board[i][col] == cell_val {
+                return false;
+            }
+        }
+
+        // Check box
+        let box_row_offset = (row % 3) * 3;
+        let box_col_offset = (col % 3) * 3;
+
+        for i in usize::from(box_row_offset)..usize::from(box_row_offset+3) {
+            for j in usize::from(box_col_offset)..usize::from(box_col_offset+3) {
+                if i != row && j != col && self.board[i][j] == cell_val {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
 
 impl Default for Sudoku {
@@ -71,35 +102,57 @@ impl Default for Sudoku {
 
 fn main() {
     test_disp();
-    test_possible();
 }
 
 // Expected output:
-// 1 2 3 | 4 5 6 | 7 8 9
-// 1 2 3 | 4 5 6 | 7 8 9
-// 1 2 3 | 4 5 6 | 7 8 9
-// ---------------------
-// 1 2 3 | 4 5 6 | 7 8 9
-// 1 2 3 | 4 5 6 | 7 8 9
-// 1 2 3 | 4 5 6 | 7 8 9
-// ---------------------
-// 1 2 3 | 4 5 6 | 7 8 9
-// 1 2 3 | 4 5 6 | 7 8 9
-// 1 2 3 | 4 5 6 | 7 8 9
-fn test_disp() {
-    let mut cur = Sudoku { ..Default::default() };
-    // Populate all rows with 1-9
-    for row in &mut cur.board {
-        *row = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    // 1 2 3 | 4 5 6 | 7 8 9
+    // 1 2 3 | 4 5 6 | 7 8 9
+    // 1 2 3 | 4 5 6 | 7 8 9
+    // ---------------------
+    // 1 2 3 | 4 5 6 | 7 8 9
+    // 1 2 3 | 4 5 6 | 7 8 9
+    // 1 2 3 | 4 5 6 | 7 8 9
+    // ---------------------
+    // 1 2 3 | 4 5 6 | 7 8 9
+    // 1 2 3 | 4 5 6 | 7 8 9
+    // 1 2 3 | 4 5 6 | 7 8 9
+    fn test_disp() {
+        let mut cur = Sudoku { ..Default::default() };
+        // Populate all rows with 1-9
+        for row in &mut cur.board {
+            *row = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        }
+        cur.display();
     }
-    cur.display();
-}
 
-// Expected output:
-// [1, 2, 3, 4, 5, 6, 7, 8, 9]
-fn test_possible() {
-    let cur = Sudoku { ..Default::default() };
-    let possible = cur.get_possible(0, 0);
-    println!("{:?}", possible);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Expected output:
+    // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    #[test]
+    fn test_possible() {
+        let cur = Sudoku { ..Default::default() };
+        let possible = cur.get_possible(0, 0);
+        let expected:Vec<u8> = [1, 2, 3, 4, 5, 6, 7, 8, 9].to_vec();
+        assert_eq!(expected, possible);
+    }
+
+    #[test]
+    fn test_verify_basic() {
+        let mut cur = Sudoku { ..Default::default() };
+        // Populate first row 1-9, remaining 2-9, 1 so verify should work
+        cur.board[0] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        for i in 1..NUM_MAX {
+            cur.board[usize::from(i)] = [2, 3, 4, 5, 6, 7, 8, 9, 1];
+        }
+
+        // Should pass since no 1 in column, row, box
+        assert_eq!(true, cur.verify_cell(0, 0));
+
+        // Second row first col should fail
+        assert_eq!(false, cur.verify_cell(1, 0));
+    }
 }
 
